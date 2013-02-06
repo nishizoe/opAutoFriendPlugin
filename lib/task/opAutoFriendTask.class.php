d4ee520f3dfb1772c777372aae5363b0db9f4c7c
kaoru nishizoe <nishizoe@tejimaya.com>
2013-02-06 17:59:36 +0900

    コーディング規約に沿ってソースを修正。

--- lib/task/opAutoFriendTask.class.php
+++ lib/task/opAutoFriendTask.class.php
@@ -11,144 +11,214 @@ class opAutoFriendTask extends sfBaseTask
 {
   protected function configure()
   {
-    $this->namespace        = 'zuniv.us';
-    $this->name             = 'AutoFriend';
+    $this->namespace        = 'opAutoFriend';
+    $this->name             = 'auto';
     $this->briefDescription = 'This plugin makes friend link automatically.';
     $this->databaseManager = null;
     $this->detailedDescription = <<<EOF
-  [./symfony zuniv.us:AutoFriend]
+  [./symfony opAutoFriend:auto]
 EOF;
 
     $this->addOption('disconnectall', null,sfCommandOption::PARAMETER_NONE, 'disconnect', null);
     $this->addOption('disconnect', null,sfCommandOption::PARAMETER_NONE, 'disconnect', null);
-    $this->addOption('member_id', null, sfCommandOption::PARAMETER_OPTIONAL, 'member_id', null);
-    $this->addOption('community_id', null, sfCommandOption::PARAMETER_OPTIONAL, 'community_id', null);
+    $this->addOption('member', null, sfCommandOption::PARAMETER_OPTIONAL, 'member', null);
+    $this->addOption('community', null, sfCommandOption::PARAMETER_OPTIONAL, 'community', null);
   }
 
   protected function execute($arguments = array(), $options = array())
   {
     $this->databaseManager = new sfDatabaseManager($this->configuration);
 
-    if($options['disconnectall']){
+    if ($options['disconnectall']){
       echo "try disconnect all\n";
       $this->disconnectall();
       echo "disconnect all done.\n";
       return;
     }
-    
-    if($options['disconnect']){
-      if($options['member_id'] && $options['community_id']){
-        $this->autoFriendDisconnectCommunity($options['member_id'],$options['community_id']);
-      }else{
+
+    if ($options['disconnect'])
+    {
+      if ($options['member'] && $options['community'])
+      {
+        $this->autoFriendDisconnectCommunity($options['member'], $options['community']);
+      }
+      else
+      {
         die('parameter requied.');
       }
       echo "disconnect\n";
+
       return;
     }
 
-    if($options['member_id']){
-      $this->autoFriendWithId($options['member_id']);
-    }else if($options['community_id']){
-      $this->autoFriendWithCommunityId($options['community_id']);
+    if ($options['member'])
+    {
+      $this->autoFriendWithId($options['member']);
+      echo "autoFriendWithMemberId\n";
+    }
+    else if ($options['community'])
+    {
+      $this->autoFriendWithCommunityId($options['community']);
       echo "autoFriendWithCommunityId\n";
-    }else{
+    }
+    else
+    {
       $this->autoFriendAll();
       echo "autoFriendAll\n";
     }
   }
-  private function autoFriendAll(){
+
+  private function autoFriendWithId($memberId = null)
+  {
+    if (!$memberId)
+    {
+      return;
+    }
+    $conn = $this->databaseManager->getDatabase(array_shift($this->databaseManager->getNames()))->getConnection();
+    //ターゲットメンバーの既存フレンドリンクを削除
+    Doctrine_Query::create()
+      ->delete()
+      ->from('MemberRelationship')
+      ->where('member_id_to = ?', $memberId)
+      ->orWhere('member_id_from = ?', $memberId)
+      ->execute();
+
+    $sql = 'insert into member_relationship (';
+    $sql .= 'member_id_to';
+    $sql .= ', member_id_from';
+    $sql .= ', is_friend';
+    $sql .= ', is_friend_pre';
+    $sql .= ') select';
+    $sql .= ' id as member_id_to';
+    $sql .= ', ? as member_id_from';
+    $sql .= ', 1 as is_friend';
+    $sql .= ', 0 as is_friend_pre';
+    $sql .= ' from member';
+    $sql .= ' where id <> ?';
+    $sql .= ' and is_login_rejected = 0';
+    $sql .= ' union select';
+    $sql .= ' ? as member_id_to';
+    $sql .= ' , id as member_id_from';
+    $sql .= ' , 1 as is_friend';
+    $sql .= ' , 0 as is_friend_pre';
+    $sql .= ' from member';
+    $sql .= ' where id <> ?';
+    $sql .= ' and is_login_rejected = 0';
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array($memberId, $memberId, $memberId, $memberId));
+  }
+
+  private function autoFriendAll()
+  {
     Doctrine_Query::create()->delete('MemberRelationship s')->execute();
-    
     $conn = $this->databaseManager->getDatabase(array_shift($this->databaseManager->getNames()))->getConnection();
 
-    $stmt = $conn->prepare('insert into member_relationship (member_id_to,member_id_from,is_friend,is_friend_pre)'
-    .' SELECT m1.id as member_id_to ,m2.id as member_id_from ,1 as is_friend , 0 as is_friend_pre FROM'
-    .' member as m1,member as m2 WHERE m1.id != m2.id AND m1.is_login_rejected = 0 AND m2.is_login_rejected = 0');
+    $sql = 'insert into member_relationship (';
+    $sql .= ' member_id_to';
+    $sql .= ', member_id_from';
+    $sql .= ', is_friend';
+    $sql .= ', is_friend_pre';
+    $sql .= ') select';
+    $sql .= ' m1.id as member_id_to';
+    $sql .= ', m2.id as member_id_from';
+    $sql .= ', 1 as is_friend';
+    $sql .= ', 0 as is_friend_pre';
+    $sql .= ' from member as m1';
+    $sql .= ', member as m2';
+    $sql .= ' where m1.id <> m2.id';
+    $sql .= ' and m1.is_login_rejected = 0';
+    $sql .= ' and m2.is_login_rejected = 0';
 
+    $stmt = $conn->prepare($sql);
     $stmt->execute();
   }
-  private function autoFriendWithCommunityId($community_id = null){
-    if(!$community_id){
+
+  private function autoFriendWithCommunityId($communityId = null)
+  {
+    if (!$communityId)
+    {
       return;
     }
-    $id_list = Doctrine_Query::create()
-    ->select('cm.member_id')->from('CommunityMember cm')
-    ->where("cm.community_id = ?",$community_id)->execute();
+    $idList = Doctrine_Query::create()
+      ->select('cm.member_id')
+      ->from('CommunityMember cm')
+      ->where("cm.community_id = ?", $communityId)
+      ->execute();
 
-    //$id_list = Doctrine::getTable('CommunityMember')->getMemberIdsByCommunityId($community_id);
-    if(count($id_list) >= 300){
-      //当面300人以上は負荷の心配があるので制限する。
-      return;
-    }
-    foreach($id_list as $id_from){
-      foreach($id_list as $id_to){
-        if($id_from['member_id'] != $id_to['member_id']){
-          $relation = Doctrine::getTable('MemberRelationship')->retrieveByFromAndTo($id_from['member_id'],$id_to['member_id']);
-          $relation2 = Doctrine::getTable('MemberRelationship')->retrieveByFromAndTo($id_to['member_id'],$id_from['member_id']);
-          if(!$relation && !$relation2){
-             $obj = new MemberRelationship();
-             $obj->member_id_from = $id_from['member_id'];
-             $obj->member_id_to = $id_to['member_id'];
-             $obj->is_friend = 1;
-             $obj->save();
-
-             $obj2 = new MemberRelationship();
-             $obj2->member_id_from = $id_to['member_id'];
-             $obj2->member_id_to = $id_from['member_id'];
-             $obj2->is_friend = 1;
-             $obj2->save();
+    foreach ($idList as $idFrom)
+    {
+      foreach ($idList as $idTo)
+      {
+        if ($idFrom['member_id'] != $idTo['member_id'])
+        {
+          $relationFromTo = Doctrine::getTable('MemberRelationship')
+            ->retrieveByFromAndTo($idFrom['member_id'],$idTo['member_id']);
+          $relationToFrom = Doctrine::getTable('MemberRelationship')
+            ->retrieveByFromAndTo($idTo['member_id'],$idFrom['member_id']);
+          if (!$relationFromTo && !$relationToFrom)
+          {
+            $this->saveMemberRelationship($idFrom['member_id'], $idTo['member_id'], 1);
+            $this->saveMemberRelationship($idTo['member_id'], $idFrom['member_id'], 1);
           }
         }
       }
     }
   }
-  private function autoFriendWithId($member_id = null){
-    if(!$member_id){
-      return;
-    }
-    $conn = $this->databaseManager->getDatabase(array_shift($this->databaseManager->getNames()))->getConnection();
-    //ターゲットメンバーの既存フレンドリンクを削除
-    Doctrine_Query::create()->delete()->from('MemberRelationship')
-      ->where('member_id_to = ?',$member_id)->orWhere('member_id_from = ?',$member_id)->execute();
 
-    $stmt = $conn->prepare('insert into member_relationship (member_id_to,member_id_from,is_friend,is_friend_pre) select id as member_id_to , ? as member_id_from, 1 as is_friend, 0 as is_friend_pre from member where id != ? AND is_login_rejected = 0 UNION select ? as member_id_to , id as member_id_from , 1 as is_friend, 0 as is_friend_pre from member where id !=? AND is_login_rejected = 0 ;');
-    $stmt->execute(array($member_id,$member_id,$member_id,$member_id));
-  }
-  private function autoFriendDisconnectCommunity($without_member_id,$community_id){
-    $id_list = Doctrine_Query::create()
-    ->select('cm.member_id')->from('CommunityMember cm')
-    ->where("cm.community_id = ?",$community_id)->execute();
+  private function autoFriendDisconnectCommunity($withoutMemberId, $communityId)
+  {
+    $idList = Doctrine_Query::create()
+    ->select('cm.member_id')
+    ->from('CommunityMember cm')
+    ->where("cm.community_id = ?", $communityId)
+    ->execute();
 
-    foreach($id_list as $id){
-      if($id['member_id'] == $without_member_id){
+    foreach ($idList as $id){
+      if($id['member_id'] == $withoutMemberId)
+      {
         //skip
-      }else{
-        $this->disconnectWitoutId($id['member_id'],$without_member_id);
+      }
+      else
+      {
+        $this->disconnectWitoutId($id['member_id'], $withoutMemberId);
       }
     }
   }
-  private function disconnectWitoutId($target_member_id,$without_member_id){
-    //ターゲットメンバーの既存フレンドリンクを削除
-    Doctrine_Query::create()->delete()->from('MemberRelationship')
-      ->where('member_id_to = ?',$target_member_id)->orWhere('member_id_from = ?',$target_member_id)->execute();
 
-    if($target_member_id == $without_member_id){
+  private function disconnectWitoutId($targetMemberId, $withoutMemberId)
+  {
+    //ターゲットメンバーの既存フレンドリンクを削除
+    Doctrine_Query::create()
+      ->delete()
+      ->from('MemberRelationship')
+      ->where('member_id_to = ?', $targetMemberId)
+      ->orWhere('member_id_from = ?', $targetMemberId)
+      ->execute();
+
+    if ($targetMemberId == $withoutMemberId)
+    {
       return;
     }
     //残すメンバーだけフレンドリンクしなおす
-    $obj = new MemberRelationship();
-    $obj->member_id_from = $target_member_id;
-    $obj->member_id_to = $without_member_id;
-    $obj->is_friend = 1;
-    $obj->save();
-
-    $obj2 = new MemberRelationship();
-    $obj2->member_id_from = $without_member_id;
-    $obj2->member_id_to = $target_member_id;
-    $obj2->is_friend = 1;
-    $obj2->save();
+    $this->saveMemberRelationship($targetMemberId, $withoutMemberId, 1);
+    $this->saveMemberRelationship($withoutMemberId, $targetMemberId, 1);
   }
-  private function disconnectall(){
+
+  private function disconnectall()
+  {
     Doctrine_Query::create()->delete('MemberRelationship')->execute();
+  }
+
+  /**
+   * MemberRelationship save
+   */
+  private function saveMemberRelationship($from, $to, $isFriend)
+  {
+    $memberRelationship = new MemberRelationship();
+    $memberRelationship->setMemberIdFrom($from);
+    $memberRelationship->setMemberIdTo($to);
+    $memberRelationship->setIsFriend($isFriend);
+    $memberRelationship->save();
   }
 }
